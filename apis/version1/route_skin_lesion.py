fb522be2b514abce37079ee89d9550f14dfac860
Công Thành <111029813+thanhmcisai@users.noreply.github.com>
2024-06-03 18:03:43 +0700

    Update route_skin_lesion.py

--- apis/version1/route_skin_lesion.py
+++ apis/version1/route_skin_lesion.py
@@ -1,12 +1,47 @@
+import filetype
 from fastapi import APIRouter
 from fastapi import UploadFile
+from fastapi import HTTPException, status
 from fastapi.responses import JSONResponse
 
 from skinlesion import YOLO as skYOLO
 
 import cv2
-import numpy as np
 import json
+import numpy as np
+from typing import IO
+
+
+def validate_file_size_type(file: IO):
+    FILE_SIZE = 5097152 # 5MB
+
+    accepted_file_types = ["image/png", "image/jpeg", "image/jpg", "image/heic", "image/heif", "image/heics", "png",
+                          "jpeg", "jpg", "heic", "heif", "heics" 
+                          ] 
+    file_info = filetype.guess(file.file)
+    if file_info is None:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unable to determine file type",
+        )
+
+    detected_content_type = file_info.extension.lower()
+
+    if (
+        file.content_type not in accepted_file_types
+        or detected_content_type not in accepted_file_types
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unsupported file type",
+        )
+
+    real_file_size = 0
+    for chunk in file.file:
+        real_file_size += len(chunk)
+        if real_file_size > FILE_SIZE:
+            raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="Too large")
+
 
 router = APIRouter()
 
@@ -21,6 +56,8 @@ def create_user(image: UploadFile):
     if not image:
         return {"message": "No upload file sent"}
 
+    validate_file_size_type(image)
+    
     img = cv2.imdecode(np.asarray(
             bytearray(image.file.read()),
             dtype="uint8"), cv2.IMREAD_COLOR)
