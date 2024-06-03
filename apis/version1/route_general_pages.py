ad44d73ec2bd3eed577c82acd6fc8e593f141690
Công Thành <111029813+thanhmcisai@users.noreply.github.com>
2024-06-04 00:35:40 +0700

    Update route_general_pages.py

--- apis/version1/route_general_pages.py
+++ apis/version1/route_general_pages.py
@@ -3,6 +3,9 @@ from fastapi import Request
 # from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 templates = Jinja2Templates(directory="templates")
 general_pages_router = APIRouter()
@@ -10,6 +13,7 @@ general_pages_router = APIRouter()
 
 @general_pages_router.get("/")
 async def home(request: Request):
+    logger.info(f"Debug: {await request.body()}")
     return templates.TemplateResponse("pages/index.html", {
         "request": request
     })
